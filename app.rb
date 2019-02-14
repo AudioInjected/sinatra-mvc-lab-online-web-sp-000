3335a977213a4066fa8eee055af1c5255bbca111
Rafael Batista <rafaelb24@yahoo.com>
2019-02-14 21:17:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :user_input
   end 
   
-  post '/' do 
+  post '/piglatinize' do 
     @user_phrase = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :piglatinize
   end 
